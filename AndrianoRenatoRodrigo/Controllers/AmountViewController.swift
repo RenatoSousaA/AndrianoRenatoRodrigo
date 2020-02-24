a77baf0aa538b3c541ca60b3fe4329ae42e7b834
Renato Sousa <renato.sousainfo@gmail.com>
2020-02-24 14:58:25 -0300

    - Criar as telas de Cadastro de Produtos, Cadastro de Estado, Lista de Compras e Total em Compras. - Constrains criadas - Controllers criadas - Importado botão para Tab - Tab criada

---
+++ AndrianoRenatoRodrigo/Controllers/AmountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AmountViewController.swift
+//  AndrianoRenatoRodrigo
+//
+//  Created by Renato Sousa on 24/02/20.
+//  Copyright © 2020 Renato Sousa. All rights reserved.
+//
+
+import UIKit
+
+class AmountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
